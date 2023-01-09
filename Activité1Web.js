a2384b4ef9edbece0df79ed586b8e6f4f1d5faf3
BLOCSCube <122237536+BLOCSCube@users.noreply.github.com>
2023-01-08 20:35:18 -0500

    Add files via upload

---
+++ Activité1Web.js
@@ -0,0 +1,12 @@
+<html> 
+	<head>
+		<meta charset="utf-8" />
+		<h1>Au revoir LFPV</h1> 
+	</head>
+	<body>
+                <h2>Lycée Francais Paul Valery</h2>
+		<p>Merci pour toutes les histoires</p>
+		<p>Que vous avez laissé</p>
+                <img src="lfpv.png" />
+	</body>
+</html> 
